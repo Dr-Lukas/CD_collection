270a05ed3d9661d9895c55df2bd0aa75a886a4d4
Dr-Lukas <l.drazdys@gmail.com>
2024-09-30 21:08:17 +0300

    Modified script to only use config as an input instead of defining them beforehand

--- config.py
+++ config.py
@@ -1,29 +1,43 @@
 from pydantic import BaseModel
-from typing import Optional, List
 import yaml
 import pathlib
 
 class Scraper(BaseModel):
-    """Defines how we query and store info related to a CPG source"""
+    """Defines URL, category being scraped, price range, and offset values used for scraping the Bengans shop website.
+    
+    Due to websites limits:
+    - batch_size: must not exceed 96.
+    - max_items_per_range: dynamically calculated based on batch_size, ensuring the total number of items stays under 10,000.
+    """
     url: str = "https://www.bengans.com/en/artiklar/cd/index.html"
     category: str = "CD"
+    price_min: float = 0.99
+    price_max: float = 600.00
+    batch_size: int = 96
+    max_offset: int = None
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.batch_size:
+            n = (10000 // self.batch_size) - 1
+            self.max_offset = self.batch_size * n
 
 class Loader(BaseModel):
-    """Defines how we query and store info related to a CPG source"""
-    path: str
+    """Defines path where data is being loaded"""
+    path: str = "bengans_scraper/load/data/"
 
 class BengansScraper(BaseModel):
-    """Defines how we query and store info related to a CPG source"""
+    """Defines how we query and store info related to a bengans website"""
     Scraper: Scraper
     Loader: Loader
 
 
 def load_config(config_path: pathlib.Path) -> BengansScraper:
-    """Loads the yaml file with the cpg import config and returns it as dict ready to initialise a
+    """Loads the yaml file with the config and returns it as dict ready to initialise a
     CprImportConfig class
     Args:
         config_path: string pointing to an existing config.yml
     """
     with open(config_path, encoding="utf-8") as f:
-        data = yaml.load(f, Loader=yaml.SafeLoader)["cpg_import"]
+        data = yaml.load(f, Loader=yaml.SafeLoader)
         return BengansScraper(**data)
--- main.py
+++ main.py
@@ -3,32 +3,38 @@ from bengans_scraper.extract import web_driver, fetch_data
 from bengans_scraper.transform import filter_parm_handler, item_parser
 from datetime import datetime
 from bengans_scraper.load import orchestration, loader
+from config import BengansScraper, load_config
 import os
 import pathlib
-import yaml
 from decimal import Decimal
 
 def dynamic_load_and_save(
-    session: requests.sessions.Session,
-    price_min: int,
-    price_max: int,
-    max_price_limit: int,
-    max_items_per_range: int,
-    filters: list,
-    output_dir: str,
-    filename: str,
-    url: str,
+    config: BengansScraper
 ) -> None:
-    genre_list = filters.get("Genre")
-    offset = 0
-    output_dir = os.path.join(output_dir, datetime.now().strftime("%Y-%m-%d"))
+    url = config["Scraper"]["url"]
+    price_min = config["Scraper"]["price_min"]
+    price_max = config["Scraper"]["price_max"]
+    batch_size = config["Scraper"]["batch_size"]
+    max_offset = config["Scraper"]["max_offset"]
+    filename = config["Loader"]["filename"]
+    output_dir = os.path.join(config["Loader"]["path"], datetime.now().strftime("%Y-%m-%d"))
     os.makedirs(output_dir, exist_ok=True)
 
-    initial_price_min = price_min
-    initial_price_max = price_max
+    raw_html = web_driver.web_driver(url)
 
+    filters = web_driver.get_genres_from_html(raw_html)
+
+    session = requests.session()
+
+    genre_list = filters.get("Genre")
+    
     if genre_list is None:
         raise Exception("Genres were not retrieved")
+    
+    offset = 0
+    initial_price_min = price_min
+    initial_price_max = price_max
+    max_price_limit = price_max
 
     for genre_item in genre_list:
         genre = genre_item.get("value")
@@ -39,14 +45,13 @@ def dynamic_load_and_save(
         price_min = initial_price_min
         price_max = initial_price_max
 
-        if match_count >= 96:
+        if match_count >= batch_size:
             while price_min < max_price_limit:
-                max_offset = (max_items_per_range // 96) * 96
                 filter_parm = filter_parm_handler.create_filter_param(price_min, price_max, genre)
 
                 test_items = fetch_data.fetch_items(session, filter_parm, max_offset)
 
-                if len(test_items) == 96:
+                if len(test_items) == batch_size:
                     if price_min >= float(test_items[-1]["current_price"]) - 0.01:
                         price_max = price_min
                         url_filtered = fetch_data.generate_url(url, price_min, price_max, genre)
@@ -71,14 +76,14 @@ def dynamic_load_and_save(
                             )
                     else:
                         print(
-                            f"Max offset {max_offset} returned 96 items, reducing price range."
+                            f"Max offset {max_offset} returned {batch_size} items, reducing price range."
                         )
                         price_max = float(test_items[-1]["current_price"]) - 0.01
                         offset = 0
                         continue
                 else:
                     print(
-                        f"Max offset {max_offset} returned no or less than 96 items, continuing to extraction."
+                        f"Max offset {max_offset} returned no or less than {batch_size} items, continuing to extraction."
                     )
                 orchestration.generate_and_save_items(
                     session,
@@ -108,35 +113,9 @@ def dynamic_load_and_save(
                 loader.save_items_to_csv(fetched_items, output_path)
 
 def main(config_path: pathlib.Path) -> None:
-    with open(config_path, "r") as yaml_file: # TODO rewrite to use config.py
-        config = yaml.safe_load(yaml_file)
+    config = load_config(config_path)
 
-    url = config["scraper"]["url"]
-
-    raw_html = web_driver.web_driver(url)
-
-    filters = web_driver.get_genres_from_html(raw_html)
-
-    price_min = 0.99
-    price_max = 600.00
-    max_price_limit = 600.00
-    max_items_per_range = 9888
-    filename = "fetched_products.csv"
-    output_dir = "bengans_scraper/load/data/"
-
-    session = requests.session()
-
-    dynamic_load_and_save(
-        session,
-        price_min,
-        price_max,
-        max_price_limit,
-        max_items_per_range,
-        filters,
-        output_dir,
-        filename,
-        url,
-    )
+    dynamic_load_and_save(config)
 
 
 if __name__ == "__main__":
